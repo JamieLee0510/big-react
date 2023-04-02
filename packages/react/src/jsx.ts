cf9db350d772ebabf49b9e8ab6318edb996999c1
Ding Yu Lee <dingyu@logflows.com>
2023-04-02 14:59:22 +0800

    feat: jsx function and shared setting

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,62 @@
+import { REACT_ELEMENT_TYPE } from "shared/ReactSymbols";
+import { ReactElement, Type, Key, Ref, Props } from "shared/ReactTypes";
+
+// 返回一個 ReactElement 的數據結構
+
+const ReactElement = (
+  type: Type,
+  key: Key,
+  ref: Ref,
+  props: Props
+): ReactElement => {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    __mark: "DingDemo",
+  };
+  return element;
+};
+
+export const jsx = (type: Type, config: any, ...maybeChildren: any) => {
+  let key: Key = null;
+  let props: Props = {};
+  let ref: Ref = null;
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === "key") {
+      if (val !== undefined) {
+        key = "" + val;
+      }
+      continue;
+    }
+    if (prop === "ref") {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+    // 判斷是不是原型的 property
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  // children 長度判斷
+  const maybeChildrenLength = maybeChildren.length;
+  if (maybeChildrenLength) {
+    if (maybeChildrenLength == 1) {
+      props.children = maybeChildren[0];
+    } else {
+      props.children = maybeChildren;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
+
+// 實際上，React在dev和prod的環境中，jsx是不同的實現
+export const jsxDEV = jsx;
