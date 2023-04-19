d6c41688a1950d132b38f62df043ae5bebf155a4
Ding Yu Lee <dingyu@logflows.com>
2023-04-19 14:11:38 +0800

    feat: single child update diff

--- packages/react-reconciler/src/childFibers.ts
+++ packages/react-reconciler/src/childFibers.ts
@@ -25,6 +25,19 @@ function ChildrenReconciler(shouldTrackSideEffect: boolean) {
       console.log(returnFiber.deletions);
     }
   }
+  function deleteRemainChildren(
+    returnFiber: FiberNode,
+    currentFirstChild: FiberNode | null
+  ) {
+    if (!shouldTrackSideEffect) {
+      return;
+    }
+    let childToDelete = currentFirstChild;
+    while (childToDelete !== null) {
+      deleteChild(returnFiber, childToDelete);
+      childToDelete = childToDelete.sibling;
+    }
+  }
 
   function reconcileSingleElement(
     returnFiber: FiberNode,
@@ -33,7 +46,7 @@ function ChildrenReconciler(shouldTrackSideEffect: boolean) {
   ) {
     // 在update階段，比較key和type，看需不需要刪除更新操作
     const key = element.key;
-    work: if (currentFiber !== null) {
+    while (currentFiber !== null) {
       // update階段
 
       if (currentFiber.key === key) {
@@ -43,21 +56,25 @@ function ChildrenReconciler(shouldTrackSideEffect: boolean) {
             // type相同，直接複用
             const existing = useFiber(currentFiber, element.props);
             existing.return = returnFiber;
+
+            // 當前節點可複用，標記剩下的節點刪除
+            deleteRemainChildren(returnFiber, currentFiber.sibling);
             return existing;
           }
-          // type不同，還是要刪除舊的
-          deleteChild(returnFiber, currentFiber);
-          break work;
+          // key相同，但type不同，還是要刪除所有舊的
+          deleteRemainChildren(returnFiber, currentFiber);
+          break;
         } else {
           // 假如不是 REACT_ELEMENT_TYPE
           if (__DEV__) {
             console.warn("還未實現的React類型：", element.$$typeof);
-            break work;
+            break;
           }
         }
       } else {
-        // 假如key不同，那就要刪掉舊的fiber，讓接下來的流程創建新的fiber
+        // key不同，那就要刪掉舊的fiber，繼續遍歷其他的sibiling
         deleteChild(returnFiber, currentFiber);
+        currentFiber = currentFiber.sibling;
       }
     }
 
@@ -86,16 +103,18 @@ function ChildrenReconciler(shouldTrackSideEffect: boolean) {
     currentFiber: FiberNode | null,
     content: string | number
   ) {
-    if (currentFiber !== null) {
+    while (currentFiber !== null) {
       // 在update流程
       if (currentFiber.tag === HostText) {
         // 類型沒有變，可以複用
         const existing = useFiber(currentFiber, { content });
         existing.return = returnFiber;
+        deleteRemainChildren(returnFiber, currentFiber.sibling);
         return existing;
       }
       // 假如類型不一樣，就代表要先刪除
       deleteChild(returnFiber, currentFiber);
+      currentFiber = currentFiber.sibling;
     }
 
     const fiber = new FiberNode(HostText, { content }, null);
